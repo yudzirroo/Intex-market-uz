a90637b3d5a3b4af64119c64c2c14b4173088b4b
abdulhadiy-1 <hadiyhadiy2008@gmail.com>
2025-07-24 16:09:33 +0500

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
-import { API } from './service/hooks/getEnv';
  
-const nextConfig: NextConfig = {
-    images: {
-    domains: ['13.61.23.60'],
-  },
-};
+const nextConfig: NextConfig = {};
  
-
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
